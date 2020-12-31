35159dfbb8f899406cf9308ca58f5b1891ed5a15
Omar Murad <ormurad5@gmail.com>
2020-12-31 00:33:27 -0500

    ...

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import store from "./store";
+import Vue from "vue"
+import App from "./App.vue"
+import store from "./store"
+import "./registerServiceWorker"
+import {
+	LayoutPlugin,
+	ModalPlugin,
+	ButtonPlugin,
+	FormPlugin,
+	FormTimepickerPlugin
+} from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(LayoutPlugin)
+Vue.use(ModalPlugin)
+Vue.use(ButtonPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormTimepickerPlugin)
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	store,
+	render: h => h(App)
+}).$mount("#app")
